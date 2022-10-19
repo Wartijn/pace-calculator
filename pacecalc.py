070fb3bded8eabd22d3715b170b93a6656dd32b8
Martijn van Sloten <martijn@vansloten.xyz>
2022-10-19 14:27:05 +0200

    update readme and help text

--- pacecalc.py
+++ pacecalc.py
@@ -15,20 +15,47 @@ Some examples:
 arg_epilog = """
 ## Distance inputs
 Distance takes a number [n], which can be an integer or a double. The following input formats are accepted: 
-- `[0]`, `[n]km` or `[n]k` for kilometers. 
-- `[n]m` for meters
+- `[n]`, `[n]km` or `[n]k` for kilometers. 
 - `[n]mi` for miles
 
 There are two pre-set distances that are not prepended by a number:
-`m` or `marathon` 
-`hm` or `"half marathon"`. This last one needs to be surrounded by quotes.  
+- `m` or `marathon` 
+- `hm` or `"half marathon"`. This last one needs to be surrounded by quotes.
 
 ## Time input
-`H:M:S`. Hours are optional, so `02:03` is 2 minutes and 3 seconds. The numbers can be zero padded, but don't have to: `01:02:03`, `1:2:3` and `1:02:3` are equivalent. 
+Pace Calculator accepts time input in two different formats: with or without letters.
+
+### Without letters
+`H:M:S`. Hours are optional, so `02:03` is 2 minutes and 3 seconds. The numbers can be zero padded, but don't have to be: `01:02:03`, `1:2:3` and `1:02:3` are equivalent. 
+
 In fact, you can add as many leading zeros as you like and minutes and seconds can be higher than 60. You can write the previous example as `000000062:03` if you like.
 
+### With letters
+To input time with letters, enter a number [n] followed by an h, m or s for hours minutes or seconds respectively. This is the format `[n]h[n]m[n]s`, where [n] is an integer and each of the number-letter combinations is optional.
+
+If the only input is either hours or minutes, they can be entered as a double (e.g `1.5m`) or as time (e.g `1:30m`). This doesn't work for seconds, which only accepts integers. 
+
+### Time input examples
+Some examples of valid inputs:
+- `01h02m03s`
+- `01:02:03`
+- `1h2m`
+- `1h3s`
+- `01:02h`
+- `2.5m`
+- `2:30`
+- `90s`
+- `.5h`
+
+These inputs are **invalid**:
+- `0.5s` (seconds are always integers)
+- `:30h` (time notation needs a number at both sides of the colon. `0:30h` and `.5h` *are* valid)
+- `30m1h` (the order of the letters can't be changed)
+- `1h30` (numbers are always followed by a letter. Add an `m` or `s` at the end, or remove all letters and enter `1:30:00`)
+- `1:30h20s` (only integers are allowed if you use multiple letters. Valid alternatives are: `1:30:20` and `1h30m20s`)
+
 ## Pace input
-`M:S` as minutes per kilometer. Both minutes and seconds are required. Just like the time input leading zeros don't matter and you can add values higher than 60. Unlike the time input, you can't add hours. 
+`M:S` as minutes per kilometer. Both minutes and seconds are required. Just like the time input, leading zeros don't matter and you can add values higher than 60. Unlike the time input, you can't add hours or use letters.
 
 # Output
 The outputs are given in this format:
@@ -36,7 +63,8 @@ The outputs are given in this format:
 - `time` in minutes and seconds. Hours are added if time >= 1 hour. Examples `5:00`, `1:05:00`
 - `pace` in minutes per kilometer. Minutes and seconds Examples: `5:00min/km`, `65:00min/km`
 
-Note: Outputs are always in kilometers. Even if the input distance is in miles or meters, pace will be in min/km.
+Note: Outputs are always in kilometers. Even if the input distance is in miles, pace will be in min/km.
+
 """
 
 
